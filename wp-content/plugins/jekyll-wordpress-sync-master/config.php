a04e067ef1091e99b24e81740473cd9a3a7b038f
dpicinat <danielpicinato@hotmail.com>
2019-03-22 14:17:14 -0300

    versãos0,2

---
+++ wp-content/plugins/jekyll-wordpress-sync-master/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// jekyll on github
+//define("JK_WP_SYNC_REPO", "");
+
+// webhook sec
+//define("JK_WP_SYNC_SECRET", "");
+
+// break 60 per-hour limit
+//define("JWS_GITHUB_TOKEN", "");
+
+
+// auto md2html when save to database
+define("JWS_AUTO_MD2HTML", true);
+define("JWS_USE_TITLE_IN_FRONTMATTER", true);
+
+// dev
+define("JWS_POST_META_SHA_KEY", "_jws_jk_sha");
+define("JWS_JK_WRONG_POST_NAME", "NOT_VALID");
