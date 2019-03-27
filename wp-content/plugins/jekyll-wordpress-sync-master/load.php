9abd69729febcf17a97524ede31454f353997c28
dpicinat <danielpicinato@hotmail.com>
2019-03-27 15:07:25 -0300

    atualização whatsapp incluso

--- wp-content/plugins/jekyll-wordpress-sync-master/load.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(dirname( __FILE__ ) . '/config.php');
-require_once(dirname( __FILE__ ) . '/class.php');
-require_once(dirname( __FILE__ ) . '/functions.php');
-
-require_once(dirname( __FILE__ ) . '/lib/Parsedown.php');
